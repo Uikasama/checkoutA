47f520d5462adb8cf35c6e7b700a07a5be4de292
Uikasama <zf000333@gmail.com>
2025-11-25 23:37:16 +0800

    Configure Vite server for external access

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/checkoutA/',   // ← 你的仓库名!!!
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+  },
 });
