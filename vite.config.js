dda13deb124916ea7fe8e308562d73efe2301fa4
macbook <fred@HUAWEI-Matebook-X-Pro.local>
2025-11-25 20:15:56 +0800

    set Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/checkoutA/',   // ⭐ 很关键：仓库名要和 GitHub 仓库一样
   plugins: [react()],
 })
